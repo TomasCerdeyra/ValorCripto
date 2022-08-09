5f0a1edd90992612ccf62700b0a19d026b60b986
Tomas Cerdeyra <tomascerdeyraimport@gmail.com>
2022-08-09 12:54:35 -0300

    comentarios y arreglo

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const url = `https://api.binance.com/api/v3/ticker/price`;
 
+//agarro la api
 async function api() {
     const res =  await fetch(url);
     const data = await res.json();
     
+    //le paso la data a las funciones
     pintarTabla(data);
     BuscarCripto(data)
 
 }
 
+//fncion para pintar la tabla
 function pintarTabla(data) {
-    celdas = ` `;
+
+    let celdas = ` `;
+    //por cada moneda hago una celda con su nombre y precio
     for (let i = 0; i < data.length; i++) {
         celdas += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`;
     }
+    //meto a celdas adentro de datos
     document.getElementById(`datos`).innerHTML = celdas;
 }
 
+//funcion para buscar una moneda
 function BuscarCripto(data) {
 
+    //agarro a la etiqueta tr para mostarla y sacarla 
     const monedas = document.getElementsByTagName(`tr`)
     const buscador = document.getElementById(`buscador`);
 
@@ -28,6 +36,7 @@ function BuscarCripto(data) {
         //Para coparar la cadena ingresada y la "i" para que no distinga entre mayusculas y minuscula
         let coparar = new RegExp(texto, "i");
 
+        //paso el valor de moneda a la variable valor y si coincide con la variable comparar que muestre sino que oculte
         for (let i = 0; i < monedas.length; i++) {
             //valor de la moneda (texto)
             let valor =monedas[i];
@@ -44,9 +53,6 @@ function BuscarCripto(data) {
     })
     
 }
-
-//buscador
-//const buscador = document.getElementById(`buscador`);
 
 
 api();
