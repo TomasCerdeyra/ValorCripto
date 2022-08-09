0ee5eac6f3f50266fb2a074b7367e6e79cd9618f
Tomas Cerdeyra <tomascerdeyraimport@gmail.com>
2022-08-09 12:46:43 -0300

    Agregado buscador

--- app.js
+++ app.js
@@ -4,17 +4,49 @@ async function api() {
     const res =  await fetch(url);
     const data = await res.json();
     
-    console.log(data);
     pintarTabla(data);
-    console.log(pintarTabla(data));
+    BuscarCripto(data)
+
 }
 
 function pintarTabla(data) {
     celdas = ` `;
     for (let i = 0; i < data.length; i++) {
-        celdas += `<td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`;
+        celdas += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`;
     }
     document.getElementById(`datos`).innerHTML = celdas;
 }
+
+function BuscarCripto(data) {
+
+    const monedas = document.getElementsByTagName(`tr`)
+    const buscador = document.getElementById(`buscador`);
+
+    buscador.addEventListener("keyup", ()=>{
+        let texto = buscador.value;
+
+        //Para coparar la cadena ingresada y la "i" para que no distinga entre mayusculas y minuscula
+        let coparar = new RegExp(texto, "i");
+
+        for (let i = 0; i < monedas.length; i++) {
+            //valor de la moneda (texto)
+            let valor =monedas[i];
+            //console.log(valor);
+            //el .test nos da un boleano si la cadena es igual o o
+            if (coparar.test(valor.innerText)) {
+                valor.classList.remove(`ocultar`);
+            } else {
+                console.log(valor);
+                valor.classList.add(`ocultar`);
+            }
+            
+        };
+    })
+    
+}
+
+//buscador
+//const buscador = document.getElementById(`buscador`);
+
 
 api();
